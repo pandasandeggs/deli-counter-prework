ac907093a83069af0217ad545bc07bad5ed4ac47
Chantal Wallace <theteatreehouse@gmail.com>
2018-07-08 03:41:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
   if array.empty?
 	  puts "The line is currently empty."
   else
-    new_line = "The lin eis currently: "
+    new_line = "The line is currently: "
 	  array.each.with_index(1) {|name,index| new_line << "#{index}. #{name} "}
 	  puts new_line
   end
