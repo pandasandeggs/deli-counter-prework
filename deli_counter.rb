1ea944bbae4da950d160af63dee4379ad932a984
Chantal Wallace <theteatreehouse@gmail.com>
2018-07-08 03:35:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli=[]
 def line(array)
   if array.empty?
 	  puts "The line is currently empty."
-  elsif
+  else
     new_line = "The current line is:"
 	  new_line << array.each_with_index(1) {|name,index| puts "#{index}. #{name}"}
   end
