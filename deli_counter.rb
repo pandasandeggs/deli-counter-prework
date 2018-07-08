0467df9f5abb607ecb591b75b35705d014fc6838
Chantal Wallace <theteatreehouse@gmail.com>
2018-07-08 04:09:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,6 +11,6 @@ def line(array)
 end
 
 def take_a_number(array,name)
+  array.push(name)
 	array.each.with_index(1) {|name,index| puts "Welcome #{name}. You are number #{index} in line."}
-	array << name
 end
