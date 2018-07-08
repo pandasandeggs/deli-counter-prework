705602f9f5d1a7f1e10c23d2049b3a0e04d8611b
Chantal Wallace <theteatreehouse@gmail.com>
2018-07-08 03:23:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,21 +11,21 @@ def line(array)
   puts new_line
 end
 
-def take_a_number(array,name)
-   new_line=[]
-  if array.empty?
-    puts "The line is currently empty."
-  else
-    new_line.push(name)
-	  array.each_with_index {|name| puts "The line is currently: #{index+1}. #{name}"}
-end
+# def take_a_number(array,name)
+#   new_line=[]
+#   if array.empty?
+#     puts "The line is currently empty."
+#   else
+#     new_line.push(name)
+# 	  array.each_with_index {|name| puts "The line is currently: #{index+1}. #{name}"}
+# end
 
-def now_serving(array)
-	if array.empty?
-	  puts "There is nobody waiting to be served."
-	else
-		array.each {|string| puts "Currently serving: #{array.shift}."}
-    end
-  end
-end
-end
+# def now_serving(array)
+# 	if array.empty?
+# 	  puts "There is nobody waiting to be served."
+# 	else
+# 		array.each {|string| puts "Currently serving: #{array.shift}."}
+#     end
+#   end
+# end
+# end
