e0d8e55444fa3e3506e5e0135513f95807282ab0
Chantal Wallace <theteatreehouse@gmail.com>
2018-07-08 03:45:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,14 +10,14 @@ def line(array)
   end
 end
 
-# def take_a_number(array,name)
-#   new_line=[]
-#   if array.empty?
-#     puts "The line is currently empty."
-#   else
-#     new_line.push(name)
-# 	  array.each_with_index {|name| puts "The line is currently: #{index+1}. #{name}"}
-# end
+def take_a_number(array,name)
+  new_line=[]
+  if array.empty?
+    puts "The line is currently empty."
+  else
+    new_line.push(name)
+	  array.each_with_index {|name| puts "The line is currently: #{index+1}. #{name}"}
+end
 
 # def now_serving(array)
 # 	if array.empty?
